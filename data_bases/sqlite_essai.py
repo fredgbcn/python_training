c30978c6a4d999441fd6644c4a7fe4a4dd38deb1
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2025-01-17 13:44:46 +0100

    essai avec sqlite3

---
+++ data_bases/sqlite_essai.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+c = conn.cursor()
+c.execute("""
+ CREATE TABLE IF NOT EXISTS employees(
+          prenom text,
+          nom text
+          )         
+          
+""")
+conn.commit()
+
+conn.close()
