d01fa3720f4fdc1d0d39f02bb8ace1132172a915
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2025-02-02 18:14:40 +0100

    checks

--- crm/crm.py
+++ crm/crm.py
@@ -1,3 +1,6 @@
+import re
+import string
+
 class User:
     def __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
         self.first_name = first_name
@@ -13,6 +16,21 @@ class User:
     def full_name(self):
         return f"{self.first_name} {self.last_name}" 
     
+    def _check_phone_number(self):
+         phone_number = re.sub(r"[+()\s]*", "", self.phone_number)
+         if len(phone_number) < 10 or not phone_number.isdigit():
+            raise ValueError(f"Numéro de téléphone {self.phone_number} invalide.")
+    def _check_names(self):
+         if not (self.first_name and self.last_name)
+            raise ValueError("Le prénom et le nom de famille ne peuvent pas êtres vides")   
+         special_characters = string.punctuation + string.digits
+         for character in self.first_name + self.last_name :
+             if character in special_characters:
+                 raise ValueError(f"Nom invalide {self.first_name}.")
+    def _checks(self):
+        self._check_phone_number()
+        self._check_names()
+
 if __name__ == "__main__":
     from faker import Faker
     fake = Faker("fr_FR")
@@ -21,6 +39,7 @@ if __name__ == "__main__":
                     last_name=fake.last_name(),
                     phone_number=fake.phone_number(),
                     address=fake.address())
+        user._checks()
         print(user)
         user.first_name = "Patrick"
         print(user.full_name)
