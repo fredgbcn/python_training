fb4c79154f2da7428a407c9df5aa9cd86855366c
Frédéric Gaufichon <fgaufichon@hotmail.fr>
2025-02-04 12:41:50 +0100

    delete and exists

--- crm/crm.py
+++ crm/crm.py
@@ -1,6 +1,6 @@
 import re
 import string
-from tinydb import TinyDB
+from tinydb import TinyDB, where
 from pathlib import Path
 
 class User:
@@ -21,6 +21,10 @@ class User:
     def full_name(self):
         return f"{self.first_name} {self.last_name}" 
     
+    @property
+    def db_instance(self):
+        return User.DB.get((where('first_name') == self.first_name) & (where('last_name') == self.last_name))
+
     def _check_phone_number(self):
          phone_number = re.sub(r"[+()\s]*", "", self.phone_number)
          if len(phone_number) < 10 or not phone_number.isdigit():
@@ -36,10 +40,23 @@ class User:
         self._check_phone_number()
         self._check_names()
 
-    def save(self, validate_data=False):
+    def exists(self):
+        return bool(self.db_instance)
+
+    def delete(self):
+        instance = self.db_instance
+        if instance and isinstance(instance, dict) and "doc_id" in instance:
+            User.DB.remove(doc_ids=[instance.doc_id])
+
+    def save(self, validate_data: bool=False) -> int:
         if validate_data:
             self._checks()
         return User.DB.insert(self.__dict__)  
+    
+def get_all_users():
+    valid_keys = {"first_name", "last_name", "phone_number", "address"}
+    return [User(**{k: user[k] for k in valid_keys if k in user}) for user in User.DB.all()]
+
 
 if __name__ == "__main__":
     from faker import Faker
@@ -55,3 +72,5 @@ if __name__ == "__main__":
         print(user.full_name)
         print(repr(user))
         print("-" *10)
+        simone = User("Simone", "Morel")
+        print(simone.delete())
